148a770955e5c8cf83c3c790c1807062882f9011
Nowshad Khan <nowshadk07@gmail.com>
2023-05-27 02:09:59 +0530

    in pop method added else clause for better understanding

--- LinkedList.py
+++ LinkedList.py
@@ -27,20 +27,27 @@ class LinkedList:
     def pop(self):
         if self.length==0:
             return None
-        temp=self.head
-        pre=self.head
-        while(temp.next):
-            pre=temp
-            temp = temp.next
-        self.tail=pre
-        self.tail.next=None
-        print("Lenght of the list ", self.length)
-        self.length-=1
-        if self.length==0:
-            self.head=None
-            self.tail=None
-        return temp.value
+        else:
+            temp=self.head
+            pre=self.head
+            while(temp.next):
+                pre=temp
+                temp = temp.next
+            self.tail=pre
+            self.tail.next=None
+            print("Lenght of the list ", self.length)
+            self.length-=1
+            if self.length==0:
+                self.head=None
+                self.tail=None
+            return temp.value
         #return temp
+    # def prepend(self,value):
+    #     if self.head==None:
+    #         self.head.value=value
+    #         self.tail.next=None
+    #     else    
+
 
 
 
