23380ea7d181f8a25801f6b9cdc05113f5095b58
Nowshad Khan <nowshadk07@gmail.com>
2023-05-27 04:36:51 +0530

    Added set_value method

--- LinkedList.py
+++ LinkedList.py
@@ -64,6 +64,34 @@ class LinkedList:
             if self.length==0:
                 self.tail=None
             return temp  #return temp.value for testing.
+
+    def get(self,value):
+        if value<=0 or value>=self.length:
+            return None
+        else:
+            temp=self.head
+            for _ in range(value):
+                temp=temp.next
+            return temp
+
+    def set_value(self,index,value):
+        #This is the code that I wrote
+        # if index<=0 or index>=self.length:
+        #     return None
+        # else:
+        #     temp=self.head
+        #     for _ in range(index):
+        #         temp=temp.next
+        #     temp.value=value    
+        # return temp.value  
+        # Below is the code shown in the course
+        temp=self.get(index)
+        if temp:
+            temp.value=value
+            return True
+        return False
+ 
+
         
 
 
@@ -75,7 +103,9 @@ my_linked_list = LinkedList(4)
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
-my_linked_list.append(2)
+my_linked_list.append(5)
+my_linked_list.append(4)
+my_linked_list.append(7)
 print("The list is now updated.")
 #my_linked_list.print_list()
 # print("First Sceario")
@@ -84,20 +114,19 @@ print("The list is now updated.")
 # print(my_linked_list.pop())
 # print("third senario")
 # print(my_linked_list.pop())
-
-#my_linked_list.prepend(56)
 my_linked_list.print_list()
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length)
+
+print("using get command")
+print(my_linked_list.get(2))
 
 
-print("poping first")
-print(my_linked_list.pop_first())
-print(my_linked_list.pop_first())
-print(my_linked_list.pop_first())
-#
 
+
+print("using set_value command")
+
+my_linked_list.set_value(2,54)
+
+my_linked_list.print_list()
 
 """
     EXPECTED OUTPUT:
