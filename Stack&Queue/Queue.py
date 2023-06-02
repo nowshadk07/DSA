eba5ff12dc5b182ab7cd2d80697e899d87319286
Nowshad Khan <nowshadk07@gmail.com>
2023-06-03 01:31:13 +0530

    Added enqueue method

--- Stack&Queue/Queue.py
+++ Stack&Queue/Queue.py
@@ -11,11 +11,26 @@ class Queue:
         self.length=1
 
     def print_queue(self):
+        print("Printing queue with length ",self.length)
         temp = self.first
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    def enqueue(self,value):
+        new_node=Node(value)
+        if self.length==0:  #if self.first is None
+            self.first=new_node
+            self.last=new_node
+        else:    
+            self.last.next=new_node
+            self.last = new_node
+        self.length+=1
+
 
 
 my_queue = Queue(4)
+my_queue.print_queue()
+
+print("Testing enqueue")
+my_queue.enqueue(3)
 my_queue.print_queue()
