85eebd058c4247be02eaf4aef5565127a20594c5
Nowshad Khan <nowshadk07@gmail.com>
2023-06-03 03:36:27 +0530

    Added file to reverse string using stack

---
+++ Stack&Queue/reverseString.py
@@ -0,0 +1,58 @@
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+
+
+
+def reverse_string(string):
+    new=''
+    my_stack=Stack()
+    for i in string:
+        my_stack.push(i)
+    for _ in string:
+        new+=my_stack.pop()
+        
+    return new  
+        
+        
+
+
+
+
+my_string = 'hello'
+
+print ( reverse_string(my_string) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    olleh
+
+"""
