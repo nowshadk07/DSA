b80d0f09c804576b2cc5b64d1a57a5f89beab431
Nowshad Khan <nowshadk07@gmail.com>
2023-06-06 00:22:23 +0530

    Added conatins method in BST

--- BST/BST.py
+++ BST/BST.py
@@ -30,14 +30,34 @@ class BinarySearchTree:
                     return True
                 temp = temp.right    
 
-
+    def conatains(self,value):
+        # if self.root is None:
+        #     return False     This Code is redundant
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp=temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
 
 
 my_tree=BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
 
 print(my_tree.root.value)
 print(my_tree.root.left.value)
 print(my_tree.root.right.value)
+
+
+print("Testing contains")
+print(my_tree.conatains(82))
+print(my_tree.conatains(17))
